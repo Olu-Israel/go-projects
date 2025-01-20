d6f6eecbc768e55845c5cfe283c53bce23859ce5
Olu-israel <olutokijuwon12@gmail.com>
2025-01-19 23:45:17 -0800

    Added the age conditionals

--- main.go
+++ main.go
@@ -6,11 +6,25 @@ import (
 
 func main() {
 	var name string
-	var quizGame string = "Fun Game"
+	var age uint
+	quizGame := "Fun Game"
 	//Greeting users
 	fmt.Printf("Hello, and welcome to %v!\n", quizGame)
 	fmt.Println("Type in your name and press Enter: ")
 	fmt.Scan(&name)
 	fmt.Printf("Hello, %v!, Let's get started!\n", name)
+	fmt.Println("Type in your age and press Enter: ")
+	fmt.Scan(&age)
+
+	//Checking if user is old enough to play
+	if age >= 18 {
+		fmt.Println("Great! You're old enough to play!")
+		fmt.Println("Let's get started!")
+
+
+
+	} else {
+		fmt.Printf("Sorry, you are not old enough to play this game.")
+	}
 
 }
