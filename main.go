2bd001e34f9b0cdafbd1230c478b0ffbbb9c5dc4
Olu-israel <olutokijuwon12@gmail.com>
2025-01-19 23:25:29 -0800

    Added user input

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 )
 
 func main() {
-	name := "Input your name: "
+	var name string
+	var quizGame string = "Fun Game"
+	//Greeting users
+	fmt.Printf("Hello, and welcome to %v!\n", quizGame)
+	fmt.Println("Type in your name and press Enter: ")
 	fmt.Scan(&name)
-	fmt.Printf("Hello, %v Welcome to take the quiz game!", name)
-	
+	fmt.Printf("Hello, %v!, Let's get started!\n", name)
+
 }
